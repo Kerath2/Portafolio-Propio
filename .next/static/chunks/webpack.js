535ec35dc45062af9c8b20b02b0da77da595a58a
kerathMac <kerath2@github.com>
2023-07-05 18:47:42 -0500

    gh pages agregado

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23069f8e537ad9a3"; }
+/******/ 		__webpack_require__.h = function() { return "9d01f4bea3c57a8f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
